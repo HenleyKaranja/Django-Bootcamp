5d373d1205a872bab26d02c1a6e89e1ae5f8bb49
HenleyKaranja <henleykaranja@gmail.com>
2026-03-14 12:38:06 +0300

    firts commint

---
+++ pyhon44.py
@@ -0,0 +1,12 @@
+#def addTwoNumber():
+  #num1 = int(input("Enter first number: "))
+  #  num2 = int(input("Enter second number: "))
+  #  print(f"The sum of {num1} and {num2} is {num1+num2}")
+
+#addTwoNumber()
+def product():
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    #result = num1 * num2
+    print(f"Product is {result}")
+product()
